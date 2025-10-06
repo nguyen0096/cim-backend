4215f9ce412f1f901fefb6e6b701e541036ca835
Neil Nguyen DN <nguyen0096@users.noreply.github.com>
2025-10-07 00:49:34 +0700

    clean up

--- internal/models/inventory.go
+++ internal/models/inventory.go
@@ -1,15 +1,5 @@
 package models
 
-// Inventory represents inventory for a product
-type Inventory struct {
-	Base
-	ProductID    uint     `json:"product_id" gorm:"unique;not null"`
-	Product      *Product `json:"product,omitempty" gorm:"foreignKey:ProductID" validate:"-"`
-	Quantity     int      `json:"quantity" gorm:"default:0"`
-	ReorderLevel int      `json:"reorder_level" gorm:"default:0"`
-	Location     string   `json:"location"`
-}
-
 // InventoryStatus represents the status of inventory
 type InventoryStatus string
 
