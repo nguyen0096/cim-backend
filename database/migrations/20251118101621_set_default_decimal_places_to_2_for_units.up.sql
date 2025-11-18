98288bc85266dd5323510ebb0eadbb026e7caf30
dotdak <dotdak@users.noreply.github.com>
2025-11-18 12:49:57 +0700

    Fix migration sql

--- database/migrations/20251118101621_set_default_decimal_places_to_2_for_units.up.sql
+++ database/migrations/20251118101621_set_default_decimal_places_to_2_for_units.up.sql
@@ -1,7 +1,7 @@
 -- Update decimal_places to 2 for all existing units
 UPDATE units
 SET decimal_places = 2
-WHERE decimal_places IS NULL OR decimal_places == 0;
+WHERE decimal_places IS NULL OR decimal_places = 0;
 
 -- Change the default value for decimal_places from 0 to 2
 ALTER TABLE units
